f1f3182dc3cfadb48577a6978fee5dd7d7a9836e
MA-antoine-33 <antoine.machet33@gmail.com>
2022-06-04 19:20:04 +0200

    created ongle-product and start a command to bought some product

---
+++ js/command.js
@@ -0,0 +1,21 @@
+let buttons = document.querySelectorAll('.product-onglet button');
+
+for (button of buttons) {
+    button.addEventListener('click', function(){
+
+    //Je recupere l'url de l'image
+    let num = this.getAttribute('data-command');
+    
+
+
+
+
+
+
+
+
+
+        //Ouvre la page pannier
+        // a rajouter à la fin pour ouvrir le lien window.location="../html/pannier.html";
+    });
+};
